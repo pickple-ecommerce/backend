ece16d71f7be97fed2f03266475594cd647db572
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-09-26 19:51:34 +0900

    init: #1 common-module 초기 생성

---
+++ common-module/src/test/java/com/pickple/common_module/CommonModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickple.common_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
