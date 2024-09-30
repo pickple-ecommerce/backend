5b1c6d624e2fa6811d0ab8621508b39690a637f4
kyungjinleelee <lv_jin@naver.com>
2024-09-30 22:30:26 +0900

    feat: #13 감사 로그를 위한 JpaAudit 클래스 파일 추가

---
+++ user-service/src/main/java/com/pickple/user/infrastructure/config/AuditAwareImpl.java
@@ -0,0 +1,20 @@
+package com.pickple.user.infrastructure.config;
+
+import jakarta.annotation.Nonnull;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class AuditAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public @Nonnull Optional<String> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return Optional.empty();
+        }
+        return Optional.of(authentication.getName());
+    }
+}
