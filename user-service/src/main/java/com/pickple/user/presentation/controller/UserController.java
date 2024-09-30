69ff7a227f8532362d51f5618dbf77a18780818c
kyungjinleelee <lv_jin@naver.com>
2024-09-30 22:55:03 +0900

    feat: #13 User 서비스 컨트롤러, 서비스, 레포 기본 파일 작성

---
+++ user-service/src/main/java/com/pickple/user/presentation/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.pickple.user.presentation.controller;
+
+import com.pickple.user.application.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+}
