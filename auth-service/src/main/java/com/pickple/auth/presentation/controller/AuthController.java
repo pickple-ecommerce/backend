39c1ee6b3c97f7298ca05d5bb9cb0f3cd49320c7
kyungjinleelee <lv_jin@naver.com>
2024-10-01 15:39:18 +0900

    feat: #7 로그인 API 구현

--- auth-service/src/main/java/com/pickple/auth/presentation/controller/AuthController.java
+++ auth-service/src/main/java/com/pickple/auth/presentation/controller/AuthController.java
@@ -1,6 +1,16 @@
 package com.pickple.auth.presentation.controller;
 
+import com.pickple.auth.application.service.AuthService;
+import com.pickple.auth.presentation.request.LoginRequestDto;
+import com.pickple.auth.presentation.request.SignUpRequestDto;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +18,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    /**
+     * 로그인
+     */
+    @PostMapping("/sign-in")
+    public ResponseEntity<?> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        try {
+            authService.login(loginRequestDto, response);
+            return ResponseEntity.ok().body("login success");
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
+        }
+    }
+
+    /**
+     *
+     * 회원 가입
+     */
     @PostMapping("/sign-up")
     public ResponseEntity<?> signup(@RequestBody @Valid SignUpRequestDto signUpDto) {
         Boolean result = authService.signup(signUpDto);
