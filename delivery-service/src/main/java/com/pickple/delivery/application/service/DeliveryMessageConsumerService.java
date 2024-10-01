218d1562f505b8e1c8bb940b05a8637ba202cdaf
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-10-01 14:04:44 +0900

    feat: #9 kafka 연동 및 docker compose 작성

---
+++ delivery-service/src/main/java/com/pickple/delivery/application/service/DeliveryMessageConsumerService.java
@@ -0,0 +1,36 @@
+package com.pickple.delivery.application.service;
+
+import static com.pickple.common_module.infrastructure.messaging.EventSerializer.objectMapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.pickple.delivery.application.dto.DeliveryCreateResponseDto;
+import com.pickple.delivery.infrastructure.messaging.events.DeliveryCreateRequestEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DeliveryMessageConsumerService {
+
+    private final DeliveryApplicationService deliveryApplicationService;
+
+    @KafkaListener(topics = "delivery-service-create")
+    public void consumeDeliveryCreation(String message) {
+        log.info("메세지 도착: {}", message);
+        try {
+            DeliveryCreateRequestEvent deliveryCreateRequestEvent = objectMapper.readValue(message,
+                    DeliveryCreateRequestEvent.class);
+            DeliveryCreateResponseDto delivery = deliveryApplicationService.createDelivery(
+                    deliveryCreateRequestEvent);
+
+            // TODO: Order 로 이벤트 발행?
+        } catch (JsonProcessingException e) {
+            // TODO: Exception Handler 구현
+            log.error("유효하지 않은 메시지 형식입니다.: {}", message, e);
+        }
+    }
+
+}
