463fe44b910433948ecb011f34bb653144433c70
easyxun <easyxun@gmail.com>
2024-10-01 18:25:48 +0900

    feat: #10 commerce 기본 엔티티 추가
    - Product 클래스의 경우, 테스트를 위해 추가함

---
+++ commerce-service/src/main/java/com/pickple/commerceservice/domain/model/Product.java
@@ -0,0 +1,41 @@
+package com.pickple.commerceservice.domain.model;
+
+import com.pickple.common_module.domain.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_products")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "product_id", updatable = false, nullable = false)
+    private UUID productId;
+
+    @Column(name = "product_name", length = 255, nullable = false)
+    private String productName;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "product_price", precision = 10, scale = 2, nullable = false)
+    private BigDecimal productPrice;
+
+    @Column(name = "product_image", length = 255)
+    private String productImage;
+
+    @Column(name = "is_public", nullable = false)
+    private Boolean isPublic = true;
+
+}
---
+++ commerce-service/src/main/java/com/pickple/commerceservice/domain/model/Vendor.java
@@ -0,0 +1,34 @@
+package com.pickple.commerceservice.domain.model;
+
+import com.pickple.common_module.domain.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_vendors")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Vendor extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "vendor_id", updatable = false, nullable = false)
+    private UUID vendorId;
+
+    @Column(name = "vendor_name", length = 255, nullable = false)
+    private String vendorName;
+
+    @Column(name = "vendor_address", length = 255, nullable = false)
+    private String vendorAddress;
+
+//    @Column(name = "user_id", nullable = false)
+//    private Long userId;
+
+}
