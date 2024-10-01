17ac9234a648060148a9e85af8428bc412697cdd
kyungjinleelee <lv_jin@naver.com>
2024-10-01 15:40:16 +0900

    feat: #13 FeignClient 통신을 위한 getUserByUsername 메서드 추가 구현

--- user-service/src/main/java/com/pickple/user/domain/repository/UserRepository.java
+++ user-service/src/main/java/com/pickple/user/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.pickple.user.domain.repository;
 import com.pickple.user.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
