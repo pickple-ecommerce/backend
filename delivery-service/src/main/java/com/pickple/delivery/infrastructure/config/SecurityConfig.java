a07096d74a2d9893db7be8e4d2dc38f7ca4a4088
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-10-03 01:00:22 +0900

    feat: #37 SecurityConfig 추가

---
+++ delivery-service/src/main/java/com/pickple/delivery/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package com.pickple.delivery.infrastructure.config;
+
+import com.pickple.delivery.infrastructure.security.CustomAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private final CustomAuthenticationFilter customAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http.sessionManagement((sessionManagement) ->
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        );
+
+        http.addFilterBefore(customAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class);
+
+        http.authorizeHttpRequests(authorize ->
+                authorize.anyRequest().authenticated()
+        );
+        return http.build();
+    }
+
+}
