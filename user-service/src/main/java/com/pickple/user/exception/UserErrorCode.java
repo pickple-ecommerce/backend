c5b374a7d6ea1f3ce44e91638dac009cf9b8b9f2
kyungjinleelee <lv_jin@naver.com>
2024-10-04 01:49:29 +0900

    feat: #13 User 모듈 커스텀 익셉션 클래스 구현 - UserErrorCode - UserExceptionHandler

---
+++ user-service/src/main/java/com/pickple/user/exception/UserErrorCode.java
@@ -0,0 +1,28 @@
+package com.pickple.user.exception;
+
+import com.pickple.common_module.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserErrorCode implements ErrorCode {
+    // ------- 4xx --------
+    FORBIDDEN(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),;
+
+
+    private final HttpStatus status;
+    private final String message;
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+}
+
