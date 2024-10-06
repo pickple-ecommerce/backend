8bb094c61b6ffff9b387de9e64ce5bae4222c234
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-10-06 15:09:05 +0900

    refactor: #39 Valid 삭제

--- delivery-service/src/main/java/com/pickple/delivery/application/dto/request/DeliveryCreateRequestDto.java
+++ delivery-service/src/main/java/com/pickple/delivery/application/dto/request/DeliveryCreateRequestDto.java
@@ -1,7 +1,5 @@
 package com.pickple.delivery.application.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,18 +8,14 @@ import lombok.Getter;
 @Builder
 public class DeliveryCreateRequestDto {
 
-    @NotNull
     private UUID orderId;
 
     private String deliveryRequirement;
 
-    @NotBlank
     private String recipientName;
 
-    @NotBlank
     private String recipientAddress;
 
-    @NotBlank
     private String recipientContact;
 
 }
