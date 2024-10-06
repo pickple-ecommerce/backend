a44394b67eeb500b26a40c378ab9ba8bcb026ff3
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-10-06 15:08:50 +0900

    feat: #39 메시지 실패 핸들러 구현

---
+++ delivery-service/src/main/java/com/pickple/delivery/exception/DeliveryMessageFailureHandler.java
@@ -0,0 +1,45 @@
+package com.pickple.delivery.exception;
+
+import com.pickple.common_module.infrastructure.messaging.EventSerializer;
+import com.pickple.delivery.application.dto.request.DeliveryCreateRequestDto;
+import com.pickple.delivery.application.events.DeliveryCreateFailureEvent;
+import com.pickple.delivery.infrastructure.messaging.events.DeliveryCreateRequestEvent;
+import com.pickple.delivery.infrastructure.messaging.events.DeliveryDeleteRequestEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DeliveryMessageFailureHandler {
+
+    @Value("${kafka.topic.delivery-create-failure}")
+    private String deliveryCreateFailureTopic;
+
+    @Value("${kafka.topic.delivery-delete-failure}")
+    private String deliveryDeleteFailureTopic;
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public void handleDeliveryCreateFailure(DeliveryCreateRequestEvent dto) {
+        log.info("Kafka 실패 메시지를 발행합니다. Topic: {}, 주문 ID: {}", deliveryCreateFailureTopic,
+                dto.getOrderId());
+        DeliveryCreateFailureEvent failureEvent = new DeliveryCreateFailureEvent(
+                dto.getOrderId(), DeliveryErrorCode.DELIVERY_CREATE_FAILURE.getMessage());
+                kafkaTemplate.send(deliveryCreateFailureTopic,
+                EventSerializer.serialize(failureEvent));
+    }
+    public void handleDeliveryDeleteFailure(DeliveryDeleteRequestEvent dto) {
+        log.info("Kafka 실패 메시지를 발행합니다. Topic: {}, 주문 ID: {}", deliveryDeleteFailureTopic,
+                dto.getOrderId());
+        DeliveryCreateFailureEvent failureEvent = new DeliveryCreateFailureEvent(
+                dto.getOrderId(), DeliveryErrorCode.DELIVERY_CREATE_FAILURE.getMessage());
+        kafkaTemplate.send(deliveryCreateFailureTopic,
+                EventSerializer.serialize(failureEvent));
+    }
+
+
+}
