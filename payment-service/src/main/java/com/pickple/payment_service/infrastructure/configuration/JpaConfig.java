ef2ea6a0d9fe7df453d59d4e3772031be224e2a8
MeGuuuun <pjm991012@gmail.com>
2024-10-09 03:46:16 +0900

    fix: #61 JpaAuditingHandler Bean 오류 해결

--- payment-service/src/main/java/com/pickple/payment_service/infrastructure/configuration/JpaConfig.java
+++ payment-service/src/main/java/com/pickple/payment_service/infrastructure/configuration/JpaConfig.java
@@ -3,10 +3,8 @@ package com.pickple.payment_service.infrastructure.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
 public class JpaConfig {
     @Bean
     public AuditorAware<String> auditorProvider() {
