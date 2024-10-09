4d0c6ef9c0d391f512846ee11b19d5b63e89e89c
kyungjinleelee <lv_jin@naver.com>
2024-10-09 19:50:47 +0900

    fix: #50 path 버그 해결 및 repository 네이티브 쿼리 삭제

--- commerce-service/src/main/java/com/pickple/commerceservice/domain/repository/StockRepository.java
+++ commerce-service/src/main/java/com/pickple/commerceservice/domain/repository/StockRepository.java
@@ -10,7 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, UUID> {
-    @Query("SELECT s FROM Stock s JOIN s.product p WHERE p.productId = :productId")
     Optional<Stock> findByProduct_ProductId(UUID productId);
 
 }
