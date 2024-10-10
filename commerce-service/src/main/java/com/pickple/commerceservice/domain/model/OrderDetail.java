c7f42203d071d5b2b9ee9b017ee5da87a2361344
easyxun <easyxun@gmail.com>
2024-10-10 11:57:34 +0900

    fix: #47 OrderDetail product 칼럼 주석 해제

--- commerce-service/src/main/java/com/pickple/commerceservice/domain/model/OrderDetail.java
+++ commerce-service/src/main/java/com/pickple/commerceservice/domain/model/OrderDetail.java
@@ -30,9 +30,9 @@ public class OrderDetail extends BaseEntity {
     @Column(name = "order_quantity", nullable = false)
     private Long orderQuantity;
 
-//    @ManyToOne
-//    @JoinColumn(name = "product_id", nullable = false)
-//    private Product product;
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
 
     @ManyToOne
     @JoinColumn(name = "order_id", nullable = false)
