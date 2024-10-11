32b2d03ca7b8e1e9a757c01e95e36d60a91f3b7c
JinMyeong Kim <kjmdkdlel@gmail.com>
2024-10-11 19:16:22 +0900

    build: #81 결제 생성 이벤트에서 message 제거

---
+++ delivery-service/src/main/java/com/pickple/delivery/infrastructure/config/RedisConfig.java
@@ -0,0 +1,47 @@
+package com.pickple.delivery.infrastructure.config;
+
+import java.time.Duration;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+
+    public static final String CACHE_PREFIX = "deliveryCache";
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new JdkSerializationRedisSerializer());
+        return template;
+    }
+
+    @Bean
+    public RedisCacheManager cacheManager(
+            RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration
+                .defaultCacheConfig()
+                .disableCachingNullValues()
+                .entryTtl(Duration.ofHours(2))
+                .computePrefixWith(CacheKeyPrefix.simple())
+                .serializeKeysWith(SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(SerializationPair.fromSerializer(new JdkSerializationRedisSerializer()));
+
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(configuration)
+                .build();
+    }
+}
