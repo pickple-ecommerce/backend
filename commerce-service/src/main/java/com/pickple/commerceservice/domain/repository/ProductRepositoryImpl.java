0dc46456c88b887fdef591975db878d7369b09e3
kyungjinleelee <lv_jin@naver.com>
2024-10-14 15:31:46 +0900

    feat: #94 키워드 기반 검색 쿼리 작성

--- commerce-service/src/main/java/com/pickple/commerceservice/domain/repository/ProductRepositoryImpl.java
+++ commerce-service/src/main/java/com/pickple/commerceservice/domain/repository/ProductRepositoryImpl.java
@@ -5,7 +5,10 @@ import com.pickple.commerceservice.domain.model.QProduct;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class ProductRepositoryImpl implements ProductRepositoryCustom{
@@ -16,6 +19,20 @@ public class ProductRepositoryImpl implements ProductRepositoryCustom{
     public Page<Product> searchByKeyword(String keyword, Pageable pageable) {
         QProduct product = QProduct.product;
 
-        return null;
+        List<Product> products = queryFactory
+                .selectFrom(product)
+                .where(product.productName.containsIgnoreCase(keyword)
+                        .and(product.isDelete.eq(false)))  // 논리적 삭제 필드 고려
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory
+                .selectFrom(product)
+                .where(product.productName.containsIgnoreCase(keyword)
+                        .and(product.isDelete.eq(false)))
+                .fetchCount();
+
+        return new PageImpl<>(products, pageable, total);
     }
 }
