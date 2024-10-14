f3b1f17712c6f77d1905696ef149681c8403fa7a
kyungjinleelee <lv_jin@naver.com>
2024-10-14 22:53:24 +0900

    feat: #96 예약구매 주문 생성 api 구현 - 일반 구매와 달리 주문 생성 시 결제, 배송은 생성되지 않음

---
+++ commerce-service/src/main/java/com/pickple/commerceservice/presentation/dto/request/PreOrderRequestDto.java
@@ -0,0 +1,10 @@
+package com.pickple.commerceservice.presentation.dto.request;
+
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class PreOrderRequestDto {
+    private UUID productId;
+}
