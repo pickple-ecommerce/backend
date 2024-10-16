d36f81465c70f04d473dec1e1a7bab9641ce8b8d
easyxun <easyxun@gmail.com>
2024-10-17 04:11:04 +0900

    feat: #90 주문 responsedto 수정 및 메서드 추가

--- commerce-service/src/main/java/com/pickple/commerceservice/application/dto/OrderResponseDto.java
+++ commerce-service/src/main/java/com/pickple/commerceservice/application/dto/OrderResponseDto.java
@@ -1,16 +1,22 @@
 package com.pickple.commerceservice.application.dto;
 
+import com.pickple.commerceservice.domain.model.Order;
 import com.pickple.commerceservice.infrastructure.feign.dto.DeliveryClientDto;
 import com.pickple.commerceservice.infrastructure.feign.dto.PaymentClientDto;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Getter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderResponseDto {
 
     private UUID orderId;
@@ -20,4 +26,18 @@ public class OrderResponseDto {
     private List<OrderDetailResponseDto> orderDetails;
     private PaymentClientDto paymentInfo;
     private DeliveryClientDto deliveryInfo;
+
+    public static OrderResponseDto fromEntity(Order order, PaymentClientDto paymentInfo, DeliveryClientDto deliveryInfo) {
+        return OrderResponseDto.builder()
+                .orderId(order.getOrderId())
+                .username(order.getUsername())
+                .amount(order.getAmount())
+                .orderStatus(order.getOrderStatus().name())
+                .orderDetails(order.getOrderDetails().stream()
+                        .map(OrderDetailResponseDto::fromEntity)
+                        .collect(Collectors.toList()))
+                .paymentInfo(paymentInfo)
+                .deliveryInfo(deliveryInfo)
+                .build();
+    }
 }
