cb695eac9bb9b4cc56a4e5d39e78aae850179580
kyungjinleelee <lv_jin@naver.com>
2024-10-16 23:50:23 +0900

    test: #96 Facade 클래스 단위테스트에서 통합테스트로 변경

--- commerce-service/src/test/java/com/pickple/commerceservice/infrastructure/facade/RedissonLockStockFacadeTest.java
+++ commerce-service/src/test/java/com/pickple/commerceservice/infrastructure/facade/RedissonLockStockFacadeTest.java
@@ -4,78 +4,61 @@ import com.pickple.commerceservice.application.service.StockService;
 import com.pickple.commerceservice.domain.model.Product;
 import com.pickple.commerceservice.domain.model.Stock;
 import com.pickple.commerceservice.domain.repository.StockRepository;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class RedissonLockStockFacadeTest {
-
-    @Mock
-    private RedissonClient redissonClient; // RedissonClient 목 객체
-    @Mock
-    private StockService stockService; // StockService 목 객체
-    @Mock
+    @Autowired
+    private RedissonLockStockFacade redissonLockStockFacade;
+    @Autowired
+    private StockService stockService;
+    @Autowired
     private StockRepository stockRepository;
-    @InjectMocks
-    private RedissonLockStockFacade redissonLockStockFacade; // Facade 객체
+    @Autowired
+    private RedissonClient redissonClient;
+
+    private Product product;
     private Stock stock;
-
-    private RLock mockLock;
-    private UUID productId;
-
+    private final UUID productId = UUID.fromString("4c3ad20e-5117-4ec6-a41f-53e00a7c117b");
+    private final UUID stockId = UUID.fromString("a7af4401-ce29-4fab-b1f1-e1906abcb07a");
     @BeforeEach
     void setUp() {
-        // RLock 목 객체 생성
-        mockLock = mock(RLock.class);
-        productId = UUID.randomUUID();
-
-        // 재고 객체 초기화
-        stock = Stock.builder()
-                .stockId(UUID.randomUUID())
-                .stockQuantity(100L) // 초기 재고 설정
+        product = Product.builder()
+                .productId(productId)
+                .productName("test 상품")
                 .build();
 
-        // RedissonClient의 getLock() 메서드 모의 설정
-        when(redissonClient.getLock("stockLock:" + productId)).thenReturn(mockLock);
+        stock = Stock.builder()
+                .stockId(stockId)
+                .stockQuantity(50L) // 초기 재고를 100개로 설정
+                .product(product)
+                .build();
+        stockRepository.saveAndFlush(stock); // 재고 저장
     }
 
     @Test
     @DisplayName("동시에 100개의 요청에서 재고 감소 테스트")
-    @Disabled
-    void decreaseStockQuantityWithLock() throws InterruptedException{
-        // Given
-        when(mockLock.tryLock(15, 1, TimeUnit.SECONDS)).thenReturn(true); // 락 획득 성공 모의
-
-        // StockService의 decreaseStockQuantity 메서드를 직접 처리하는 로직 추가
-        doAnswer(invocation -> {
-            // 재고 수량을 감소시키는 로직
-            stock.updateStockQuantity(stock.getStockQuantity() - 1);
-            return null;
-        }).when(stockService).decreaseStockQuantity(productId);
-
+    void decreaseStockQuantityWithLock() throws InterruptedException {
         int threadCount = 100;
         ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
         CountDownLatch latch = new CountDownLatch(threadCount);
 
-        // When
+        // 100개의 스레드를 사용해 동시 재고 감소 요청을 보냄
         for (int i = 0; i < threadCount; i++) {
             executorService.submit(() -> {
                 try {
@@ -90,6 +73,9 @@ class RedissonLockStockFacadeTest {
         latch.await();
 
         // Then (재고 차감 검증)
-        assertEquals(0L, stock.getStockQuantity()); // 재고가 100개에서 0개로 정확히 차감되었는지 확인
+        Optional<Stock> updatedStock = stockRepository.findById(stockId);
+        assertEquals(0L, updatedStock.orElseThrow().getStockQuantity()); // 재고가 50개에서 0개로 정확히 차감되었는지 확인
     }
+
+
 }
