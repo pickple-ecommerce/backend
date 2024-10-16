f64b1faca8f33a7e12081692a1f1a2018684f2a9
easyxun <easyxun@gmail.com>
2024-10-17 04:08:44 +0900

    feat: #90 배송 완료 시 주문 상태 변경

--- commerce-service/src/main/java/com/pickple/commerceservice/application/service/OrderEventService.java
+++ commerce-service/src/main/java/com/pickple/commerceservice/application/service/OrderEventService.java
@@ -19,6 +19,9 @@ public class OrderEventService {
     private final OrderRepository orderRepository;
     private final OrderMessagingProducerService messagingProducerService;
 
+    /**
+     * 결제 완료 처리 후 배송 요청을 보내는 메서드
+     */
     @Transactional
     public void handlePaymentComplete(UUID orderId, UUID paymentId, String username) {
         Order order = orderRepository.findById(orderId).orElseThrow(() -> new CustomException(
@@ -29,11 +32,27 @@ public class OrderEventService {
         messagingProducerService.sendDeliveryCreateRequest(orderId, username);
     }
 
+    /**
+     * 배송 생성 완료 처리 메서드
+     */
     @Transactional
     public void handleDeliveryCreate(UUID orderId, UUID deliveryId) {
         Order order = orderRepository.findById(orderId).orElseThrow(() -> new CustomException(
                 CommerceErrorCode.ORDER_NOT_FOUND));
         order.assignDeliveryId(deliveryId);
         orderRepository.save(order); // order 조회 test 목적
+    }
+
+    /**
+     * 배송 완료 시 주문 상태를 COMPLETED로 변경하는 메서드
+     */
+    @Transactional
+    public void handleDeliveryEnd(UUID orderId) {
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(() -> new CustomException(CommerceErrorCode.ORDER_NOT_FOUND));
+
+        // 주문 상태를 COMPLETED로 변경
+        order.changeStatus(OrderStatus.COMPLETED);
+        orderRepository.save(order);
     }
 }
