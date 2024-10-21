de26887cf056e2a23ca0426967c727bae29878a8
easyxun <easyxun@gmail.com>
2024-10-22 05:56:58 +0900

    refactor: #123 feign 호출 시 실패할 경우에 대한 fallback 처리

--- commerce-service/src/main/java/com/pickple/commerceservice/infrastructure/feign/dto/DeliveryClientDto.java
+++ commerce-service/src/main/java/com/pickple/commerceservice/infrastructure/feign/dto/DeliveryClientDto.java
@@ -1,14 +1,14 @@
 package com.pickple.commerceservice.infrastructure.feign.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.UUID;
 
 @Getter
-@Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class DeliveryClientDto {
     private UUID deliveryId;
     private UUID orderId;
--- commerce-service/src/main/java/com/pickple/commerceservice/infrastructure/feign/dto/PaymentClientDto.java
+++ commerce-service/src/main/java/com/pickple/commerceservice/infrastructure/feign/dto/PaymentClientDto.java
@@ -2,12 +2,14 @@ package com.pickple.commerceservice.infrastructure.feign.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.UUID;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class PaymentClientDto {
     private UUID orderId;
     private UUID paymentId;
